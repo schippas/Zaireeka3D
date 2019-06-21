b28362747f6a7aecfaa36393859e9ccaaba6467e
schippas <schippas@purdue.edu>
2019-06-21 18:58:57 -0400

    Keep grabbed objects from getting stuck off camera
    Before, grabbed objects could be moved out of the center of a player's view, which would make it difficult to drop them. This prevents that, by checking if the object is in the center of the screen.

--- Zaireeka3D/Assets/Scripts/MoveObject.cs
+++ Zaireeka3D/Assets/Scripts/MoveObject.cs
@@ -9,21 +9,36 @@ public class MoveObject : MonoBehaviour
     float distance;
     float force = 100;
         
-    public int hold = 0;
+    public bool hold = false;
     public GameObject item;
     public GameObject player;
     public GameObject view;
+    public Camera cam;
+
+    private void Start()
+    {
+        cam = view.GetComponent<Camera>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(hold == 1)
+        if(hold == true)
         {
             item.GetComponent<Rigidbody>().velocity = Vector3.zero;
             item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             item.transform.SetParent(view.transform);
             item.GetComponent<Rigidbody>().velocity = player.GetComponent<Rigidbody>().velocity;
             item.GetComponent<Rigidbody>().angularVelocity = player.GetComponent<Rigidbody>().angularVelocity;
+
+            Ray ray = cam.ViewportPointToRay((new Vector3(.5f, .5f, 0)));
+            RaycastHit hit;
+            Vector2 viewPos = cam.WorldToViewportPoint(item.transform.position);
+            Vector2 center = new Vector3(0.5f,0.5f);
+            if ((Vector2.Distance(viewPos, center) > 1) && (!(Physics.Raycast(ray, out hit))) || ((Physics.Raycast(ray, out hit)) && (hit.collider != item.GetComponent<Collider>())))
+            {
+                DropObject();
+            }
         }
         else
         {
@@ -35,13 +50,13 @@ public class MoveObject : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if((Input.GetMouseButtonDown(0)) && (hold == 0)){
+        if((Input.GetMouseButtonDown(0)) && (hold == false)){
             GrabObject();
-        }else if((Input.GetMouseButtonDown(0)) && (hold == 1))
+        }else if((Input.GetMouseButtonDown(0)) && (hold == true))
         {
             DropObject();
         }
-        if((Input.GetMouseButtonDown(1)) && (hold == 1))
+        if((Input.GetMouseButtonDown(1)) && (hold == true))
         {
             ThrowObject();
         }
@@ -49,20 +64,20 @@ public class MoveObject : MonoBehaviour
 
     void GrabObject()
     {
-        hold = 1;
+        hold = true;
         item.GetComponent<Rigidbody>().detectCollisions = true;
         item.GetComponent<Rigidbody>().useGravity = false;
     }
 
     void DropObject()
     {
-        hold = 0;
+        hold = false;
         item.GetComponent<Rigidbody>().useGravity = true;
     }
 
     void ThrowObject()
     {
-        hold = 0;
+        hold = false;
         item.GetComponent<Rigidbody>().useGravity = true;
     }
 }
