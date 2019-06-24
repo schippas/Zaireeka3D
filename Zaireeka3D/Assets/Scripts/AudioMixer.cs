bf8a261283ea4c13c4394e0644b7c896de7df03f
schippas <schippas@purdue.edu>
2019-06-24 19:14:09 -0400

    Time scrollbar for individual audio sources

--- Zaireeka3D/Assets/Scripts/AudioMixer.cs
+++ Zaireeka3D/Assets/Scripts/AudioMixer.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioMixer : MonoBehaviour
 {
     public GameObject source;
+    public GameObject timeSlider;
+
+    private void Start()
+    {
+        if (timeSlider)
+        {
+            setLength();
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (source.GetComponent<AudioSource>().clip)
+        {
+            timeSlider.GetComponent<Slider>().value = source.GetComponent<AudioSource>().time;
+        }
     }
 
     public void TogglePlay()
@@ -23,6 +36,14 @@ public class AudioMixer : MonoBehaviour
             source.GetComponent<AudioSource>().Play();
         }
 
+    }
+
+    public void setLength()
+    {
+        if (source.GetComponent<AudioSource>().clip)
+        {
+            timeSlider.GetComponent<Slider>().maxValue = source.GetComponent<AudioSource>().clip.length;
+        }
     }
 
 
