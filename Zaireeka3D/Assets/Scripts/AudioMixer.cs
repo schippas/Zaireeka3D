d9ae430113dcf2df4b54b09104db39bc826f52d3
schippas <schippas@purdue.edu>
2019-06-24 12:38:23 -0400

    Added Pause audio button for individual sources

---
+++ Zaireeka3D/Assets/Scripts/AudioMixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMixer : MonoBehaviour
+{
+    public GameObject source;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TogglePlay()
+    {
+        if (source.GetComponent<AudioSource>().isPlaying)
+        {
+            source.GetComponent<AudioSource>().Pause();
+        }
+        else
+        {
+            source.GetComponent<AudioSource>().Play();
+        }
+
+    }
+
+}
---
+++ Zaireeka3D/Assets/Scripts/OpenAudioMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class OpenAudioMenu : MonoBehaviour
+{
+
+    public GameObject modalPanel;
+    public GameObject source;
+
+    public GameObject volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        modalPanel.SetActive(false);
+        volumeSlider.GetComponent<Slider>().onValueChanged.Equals(source);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
